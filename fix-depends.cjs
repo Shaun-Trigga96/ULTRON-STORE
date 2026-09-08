d8a6bb009f06e7127981647ca3d70fabe55cc1d0
Thabiso Matsaba <thabisomatsaba1976@gmail.com>
2026-09-08 22:32:11 +0000

    chore: remove temporary migration and setup scripts

--- fix-depends.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('docker-compose.yml', 'utf8');
-
-const regex = /(    environment:\n      - PORT=4001[\s\S]*?      - DB_PASSWORD=devsecret\n)/;
-content = content.replace(regex, `$1    depends_on:\n      - redis\n      - postgres\n`);
-fs.writeFileSync('docker-compose.yml', content);
